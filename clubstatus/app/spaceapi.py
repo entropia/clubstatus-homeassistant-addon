64860210c3d8632c2d4e4d4656f69d780fdc9b67
Henry <henry@stored.cc>
2025-10-13 16:52:29 +0200

    initial commit

---
+++ clubstatus/app/spaceapi.py
@@ -0,0 +1,151 @@
+#!/usr/bin/env python3
+# coding=utf-8
+import paho.mqtt.client as mqtt
+from flask import Flask, Response
+import json, time, os, threading
+
+debug = bool(os.getenv('DEBUG'))
+mqtt_topic = str(os.getenv('MQTT_TOPIC'))
+mqtt_broker = str(os.getenv('MQTT_BROKER'))
+mqtt_broker_port = int(os.getenv('MQTT_BROKER_PORT'))
+app = Flask(__name__)
+
+class Clubstatus:
+    def __init__(self):
+        self.spaceapi_status = None
+        self.spaceapi_html_status = "Unbekannt"
+        self.spaceapi_output = {}
+        self.spaceapi_human_output = ""
+
+clubstatus = Clubstatus()
+
+def on_connect(client, userdata, flags, rc):
+    if debug:
+        print("Connected with result code "+str(rc))
+    client.subscribe(mqtt_topic)
+
+def on_message(client, userdata, msg):
+    if debug:
+        print(msg.topic + " " + str(msg.payload))
+
+    status_payload = msg.payload.decode()
+    timestamp = int(time.time())
+
+    if status_payload == "1":
+        clubstatus.spaceapi_status = True
+        clubstatus.spaceapi_html_status = "Offen"
+    elif status_payload == "0":
+        clubstatus.spaceapi_status = False
+        clubstatus.spaceapi_html_status = "Geschlossen"
+    else:
+        clubstatus.spaceapi_status = None
+        clubstatus. spaceapi_html_status = "Unbekannt"
+
+    clubstatus.spaceapi_output = {
+        "api_compatibility": [ "14" ],
+        "contact": {
+            "email": "info@entropia.de",
+            "irc": "irc://irc.hackint.eu/#entropia",
+            "issue_mail": "spaceapi@entropia.de",
+            "mastodon": "@entropia@chaos.social",
+            "matrix": "#entropia:entropia.de",
+            "ml": "news@list.entropia.de",
+            "phone": "+49 721 5604732",
+            "twitter": "@entropiagpn"
+        },
+        "feeds": {
+            "wiki": {
+                "type": "html",
+                "url": "https://entropia.de/Hauptseite"
+            },
+            "calendar": {
+                "type": "html",
+                "url": "https://entropia.de/Vorlage:Termine"
+            }
+        },
+        "location": {
+            "address": ("Entropia e.V., Gewerbehof, "
+                        "Steinstraße 23, 76133 Karlsruhe, Germany"),
+            "lat": 49.0067,
+            "lon": 8.407438,
+            "timezone": "Europe/Berlin"
+        },
+        "membership_plans": [
+            {
+                "billing_interval": "monthly",
+                "currency": "EUR",
+                "description": "Normale Mitglieder gem. https://entropia.de/Satzung_des_Vereins_Entropia_e.V.#Beitragsordnung",
+                "name": "Regular Members",
+                "value": 25,
+            },
+            {
+                "billing_interval": "monthly",
+                "currency": "EUR",
+                "description": "Mitglieder des CCC e.V. gem. https://entropia.de/Satzung_des_Vereins_Entropia_e.V.#Beitragsordnung",
+                "name": "Members of CCC e.V.",
+                "value": 19,
+            },
+            {
+                "billing_interval": "monthly",
+                "currency": "EUR",
+                "description": "Schüler, Studenten, Auszubildende und Menschen mit geringem Einkommen gem. https://entropia.de/Satzung_des_Vereins_Entropia_e.V.#Beitragsordnung",
+                "name": "Reduced Fee Members",
+                "value": 15,
+            },
+            {
+                "billing_interval": "monthly",
+                "currency": "EUR",
+                "description": "Fördermitglieder gem. https://entropia.de/Satzung_des_Vereins_Entropia_e.V.#Beitragsordnung",
+                "name": "Sustaining Membership",
+                "value": 6,
+            }
+        ],
+        "logo": "https://entropia.de/wiki/images/e/ed/Teebeutel1_noev.png",
+        "spacefed": {
+            "spacenet": False,
+            "spacesaml": False
+        },
+        "state": {
+            "icon": {
+                "closed": "https://entropia.de/wiki/images/7/76/Clubstatus_zu.png",
+                "open": "https://entropia.de/wiki/images/7/7a/Clubstatus_offen.png"
+            },
+            "lastchange": timestamp,
+            "open": clubstatus.spaceapi_status
+        },
+        "space": "Entropia",
+        "url": "https://entropia.de/"
+    }
+
+    clubstatus.spaceapi_human_output = ("<!-- this is not meant to be machine-readable. please use /spaceapi or /status.json -->"
+             "<html>"
+                "<head><title>Clubstatus</title></head>"
+                f"<body>{clubstatus.spaceapi_html_status}</body>"
+             "</html>"
+             )
+
+    if debug:
+        print(f"HTML: \n{clubstatus.spaceapi_human_output}")
+        print("JSON: \n"+json.dumps(clubstatus.spaceapi_output, indent=2))
+
+
+def mqtt_thread():
+    client = mqtt.Client()
+    client.on_connect = on_connect
+    client.on_message = on_message
+    client.connect(mqtt_broker, mqtt_broker_port)
+    client.loop_forever()
+
+thread = threading.Thread(target=mqtt_thread)
+thread.daemon = True
+thread.start()
+
+@app.route("/")
+def human():
+    return clubstatus.spaceapi_human_output
+
+@app.route("/spaceapi")
+def spaceapi():
+    return json.dumps(clubstatus.spaceapi_output)
+
+app.run(host='0.0.0.0', port=5000)
